a6ffbb0bd15217a9880259b0795380c8c53c8227
BaratovSokhibjon <sokhibjonbaratov@gmail.com>
2023-05-04 23:08:03 +0900

    //Lab9/Exercise 1 and 2
    Added Exercise 1 and 2 and some more libraries as well as one more function outside the main function hoping this won't affect the code performance

--- main.cpp
+++ main.cpp
@@ -1,9 +1,18 @@
+
+#define _USE_MATH_DEFINES
 #include <iostream>
 #include <string>
 #include <cmath>
 #include <iomanip>
+#include <ctime>
+#include <cstdlib>
+
 using namespace std;
+
 //#define PI (3.14)
+
+int get_random();
+
 int main() {
     /*
     char str[] = "Do not interrupt me!";
@@ -112,6 +121,86 @@ float a,b,c;
    cout << a << " * " << b << " = " << a*b << endl;
    cout << float(a) << " / " << float(b) << " = " << float(a)/float(b) << endl;
 */
-   return 0;
 
+   /*
+   //Lab4/Exercise1
+   int a;
+   cout << "Input the current temperature: ";
+   cin >> a;
+   if (a<0)
+   {
+      cout << "Freezing weather";
+   }else if (a <);
+   {
+   }
+   */
+
+  /*
+  //Lab9 Exercise 1
+	int i;
+	for (i = 0; i <= 360; i++) {
+
+		double i_rad = i / 180.0 * M_PI;
+
+		double x = 10 * (cos(i_rad)); //x = r (cos (degrees°)), 
+		double y = 10 * (sin(i_rad)); //y = r (sin (degrees°))
+
+		
+		
+
+		cout << i << ": " << x << ", " << y << endl;
+	}
+	
+   */
+
+  /*
+   //Lab9/Exercise 2
+       srand(time(NULL));
+       int d[7] = {0};
+
+       for (int i = 1; i <= 1000000; i++)
+       {
+          int a = get_random();
+
+          switch (a)
+          {
+          case 1:
+             d[1]++;
+             break;
+          case 2:
+             d[2]++;
+             break;
+          case 3:
+             d[3]++;
+             break;
+          case 4:
+             d[4]++;
+             break;
+          case 5:
+             d[5]++;
+             break;
+          case 6:
+             d[6]++;
+             break;
+          }
+       }
+
+       for (int j = 1; j <= 6; j++)
+       {
+          double perc = (static_cast<double>(d[j]) / 1000000) * 100;
+
+          cout << j << ": " << d[j] << ", ";
+          cout.precision(2);
+          cout << fixed;
+          cout << perc << "%" << endl;
+       }
+   
+
+    return 0;
 }
+*/
+
+ int get_random()
+    {
+       return rand() % 6 + 1;
+    }
