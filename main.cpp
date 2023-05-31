e1692f92a2607a7c2365ba52ea6649f12dbb9208
BaratovSokhibjon <sokhibjonbaratov@gmail.com>
2023-05-31 10:50:33 +0900

    Lab 12 finished

--- main.cpp
+++ main.cpp
@@ -1,50 +1,26 @@
 #include <iostream>
-#include <string>
 
 using namespace std;
 
-struct date {
+struct date
+{
     int year;
     int month;
     int day;
 };
 
-struct student {
-    int id;
-    date dob;
-    date grad;
-};
+int main(){
+struct date date;
 
-student getStudentInfo() {
-    student s;
+    cout << "Enter your date of birth: " << endl;
+    cout << "Year: ";
+    cin >> date.year;
+    cout << "Month: ";
+    cin >> date.month;
+    cout << "Day: ";
+    cin >> date.day;
 
-    cout << "Student ID: ";
-    cin >> s.id;
-    cout << "Date of birth (Y M D): ";
-    cin >> s.dob.year >> s.dob.month >> s.dob.day;
-    cout << "Expected graduation date: ";
-    cin >> s.grad.year >> s.grad.month >> s.grad.day;
+    string months[12] = {"January", "February", "March", "April", "May", "June","July", "August", "September", "October", "November", "December"};
 
-    return s;
-}
-
-void displayStudentInfo(const student& s) {
-    string months[12] = {"January", "February", "March", "April", "May", "June",
-                         "July", "August", "September", "October", "November", "December"};
-    cout << "Student ID: " << s.id << endl;
-    cout << "Date of birth: " << months[s.dob.month - 1] << " " << s.dob.day << ", " << s.dob.year << endl;
-    cout << "Expected graduation date: " << months[s.grad.month - 1] << " " << s.grad.day << ", " << s.grad.year << endl;
-}
-
-int main()
-{
-    // Student={id,BoD{Y,M,D},Graduation{Y,M,D}}
-    student s1 = {123, {2000, 1, 1}, {2026, 2, 28}};
-    cout << "Init Student Info... ==>" << endl;
-    displayStudentInfo(s1);
-    cout << endl << "Input the new student information: " << endl;
-    s1 = getStudentInfo();
-    cout << endl << "Received Student Info... ==>" << endl;
-    displayStudentInfo(s1);
-    return 0;
+    cout << endl << "Your Date of birth: " << months[date.month - 1 ] << " " << date.day << ", " << date.year;
 }
